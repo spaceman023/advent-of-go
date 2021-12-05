14aae7d9cf6e684e02ba5a4a12ccad886c5bd886
John Richardson <jkrichardson08@gmail.com>
2021-12-05 09:55:42 -0600

    solvedday4

--- day4.js
+++ day4.js
@@ -1,5 +1,5 @@
 //js again. I will come back and do these in Go...eventually.
-
+//parse input
 let input = document.getElementsByTagName("pre")[0].innerText;
 input = input.split("\n");
 let moves = input[0].split(",").map((i) => Number(i));
@@ -19,3 +19,83 @@ for (let i = 2; i < input.length; i++) {
         .map((i) => Number(i));
     currentboard.push(build);
 }
+
+//functions
+function checkBoard(board) {
+    //check row
+
+    for (let i = 0; i < board.length; i++) {
+        bingoCheck = true;
+        for (let j = 0; j < board[i].length; j++) {
+            let cell = board[i][j];
+            if (cell !== true) {
+                bingoCheck = false;
+                break;
+            }
+        }
+        if (bingoCheck === true) {
+            console.log("bingo");
+            return true;
+        }
+    }
+    //check columns
+    for (let i = 0; i < board[0].length; i++) {
+        bingoCheck = true;
+        for (let j = 0; j < board.length; j++) {
+            let cell = board[j][i];
+            if (cell !== true) {
+                bingoCheck = false;
+                break;
+            }
+        }
+        if (bingoCheck === true) {
+            return true;
+            console.log("bingo");
+        }
+    }
+    return false;
+}
+function markBoard(board, num) {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            let cell = board[i][j];
+            if (cell === num) {
+                board[i][j] = true;
+            }
+        }
+    }
+    return board;
+}
+
+function sumBoard(board) {
+    sum = 0;
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            let cell = board[i][j];
+            if (cell !== true) {
+                sum += cell;
+            }
+        }
+    }
+    return sum;
+}
+
+function solve(boards, moves) {
+    sums = [];
+    for (let i = 0; i < moves.length; i++) {
+        move = moves[i];
+        for (let j = 0; j < boards.length; j++) {
+            let board = boards[j];
+            //console.log(board);
+            board = markBoard(board, move);
+            //console.log(board);
+            if (checkBoard(board)) {
+                sums.push(sumBoard(board) * move);
+                boards.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return sums;
+}
+console.log(solve(boards, moves));

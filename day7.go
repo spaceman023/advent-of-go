002e5e1bd8834d7b157c4cf0db9a0b13724241cd
John Richardson <jkrichardson08@gmail.com>
2021-12-07 21:40:40 -0600

    refactor day 7

--- day7.go
+++ day7.go
@@ -1,4 +1,4 @@
-//tried to go fast but am too stupid - will refactor this tomorrow
+//refactored!
 
 package day7
 
@@ -14,25 +14,9 @@ import (
 )
 
 var inputFile = flag.String("inputFile", "inputs/input7.txt", "Relative file path to use as input.")
-var factVal = 1
 
-func main() {
-	start := time.Now()
-	// Code to measure
-
-	flag.Parse()
-	bytes, err := ioutil.ReadFile(*inputFile)
-	if err != nil {
-		return
-	}
-	contents := string(bytes)
-	positions := []int{}
-	for _, v := range strings.Split(contents, ",") {
-		intv, _ := strconv.Atoi(v)
-		positions = append(positions, intv)
-	}
+func getMedian(positions []int) int {
 	sort.Ints(positions)
-
 	middle := len(positions) / 2
 	medianValue := 0
 	if len(positions)%2 == 1 {
@@ -40,27 +24,49 @@ func main() {
 	} else {
 		medianValue = (positions[middle-1] + positions[middle]) / 2
 	}
-	fuel := 0
+	return medianValue
+}
+
+func getAverage(positions []int) int {
 	sum := 0
 	for i := 0; i < len(positions); i++ {
 		sum += positions[i]
 	}
-	avg := math.Floor(float64(sum) / float64(len(positions)))
+	avg := int(math.Floor(float64(sum) / float64(len(positions))))
+	return avg
+}
+
+func calculateFuel2(positions []int, point int) int {
+	fuel := 0
 	for _, v := range positions {
-		diff := int(math.Abs(float64(v) - float64(avg)))
-		total := 0
-		for i := 0; i <= diff; i++ {
-			total += i
-		}
-		fuel += total
+		diff := int(math.Abs(float64(v) - float64(point)))
+		fuel += (diff * (diff + 1)) / 2
 	}
-	//fmt.Println(avg)
-	fmt.Println(medianValue)
-	fmt.Println(fuel)
+	return fuel
+}
+
+func calculateFuel(positions []int, point int) int {
+	fuel := 0
+	for _, v := range positions {
+		diff := int(math.Abs(float64(v) - float64(point)))
+		fuel += diff
+	}
+	return fuel
+}
+
+func main() {
+	start := time.Now()
+	flag.Parse()
+	bytes, _ := ioutil.ReadFile(*inputFile)
+	contents := string(bytes)
+	positions := []int{}
+	for _, v := range strings.Split(contents, ",") {
+		intv, _ := strconv.Atoi(v)
+		positions = append(positions, intv)
+	}
+	partOne := calculateFuel(positions, getMedian(positions))
+	partTwo := calculateFuel2(positions, getAverage(positions))
+	fmt.Printf("Part one: %v \nPart two: %v \n", partOne, partTwo)
 	duration := time.Since(start)
-
 	fmt.Println(duration)
-
-	// Nanoseconds as int64
-	//fmt.Println(duration.Nanoseconds())
 }
